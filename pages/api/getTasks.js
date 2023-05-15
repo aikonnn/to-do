296f8917b2b10e1f3ba76119e85bf012b5665312
aikonnn <nokia_tuk@hotmail.com>
2023-05-15 15:59:37 +0700

    concealed .env

--- pages/api/getTasks.js
+++ pages/api/getTasks.js
@@ -1,6 +1,6 @@
 const {Client} = require("pg");
 
-const client = new Client("postgres://postgres:pass@" + process.env.BASE_ADDR +":5432/todo");
+const client = new Client(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.BASE_ADDR}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
 
 client.connect();
 
