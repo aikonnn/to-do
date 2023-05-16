b2feda5ed83af862db2360cd96ca36025dd96460
aikonnn <nokia_tuk@hotmail.com>
2023-05-16 14:33:13 +0700

    prune unneeded console logs

--- pages/api/task/[taskid].js
+++ pages/api/task/[taskid].js
@@ -6,10 +6,7 @@ client.connect();
 
 
 export default async function handleTasks(req, res) {
-    console.log("calling taskk endpoint")
     if(req.method == 'DELETE'){
-        console.log("called delete");
-        console.log(req.query.taskid);
         const ans = await client.query(`DELETE from tasks where id='${req.query.taskid}'`);
         res.status(200).json({
             status: 'success'
--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -7,13 +7,11 @@ client.connect();
 
 export default async function addTasks(req, res) {
     if(req.method == 'POST'){
-        console.log("adding task");
         const ans = await client.query("INSERT INTO tasks(user_id, task, urgency) values($1::uuid, 'write your task here' ,1)", [req.body.userid]);
         res.status(200).json({
         status: 'success'
         });
     } else if(req.method == 'PUT'){
-        console.log("editing task");
         const ans = await client.query("UPDATE tasks SET task = $1, urgency = $2 WHERE id = $3", [req.body.task, req.body.urgency, req.body.taskid]);
         res.status(200).json({
         status: 'success'
