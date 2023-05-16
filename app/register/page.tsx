4a0e20a0a7bd266e7e49076d3c73db1585065239
aikonnn <nokia_tuk@hotmail.com>
2023-05-16 14:42:32 +0700

    display regis error

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Register() {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const [error, setError] = useState('');
 
-  async function handleSubmit(){
+  async function handleSubmit(e: React.FormEvent){
+    e.preventDefault();
+
     const data = await (
       await fetch(
         "/api/users",
@@ -22,12 +25,17 @@ export default function Register() {
         }
       )
     ).json();
+
+    if(data.error){
+      setError("registration failed, use a different email")
+    }
   }
 
   return (
     <main className="flex h-screen flex-col items-center justify-center p-24">
+      {error != '' && <div className='bg-red-500 px-3 py-2 rounded-xl'>{error}</div>}
       <div className='py-5'>register</div>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+      <form onSubmit={e=>handleSubmit(e)} className="flex flex-col items-center justify-center">
       <input onChange={(e)=>{setEmail(e.target.value)}} placeholder='email' className='my-2 px-3 py-1 rounded-md text-black focus:bg-black-100' type="email" required></input>
       <input onChange={(e)=>{setPass(e.target.value)}} placeholder='password' className='my-2 px-3 py-1 rounded-md text-black focus:bg-black-100' type='password' required></input>
       <button type='submit' className='transition m-3 bg-blue-200 py-1 px-3 text-black rounded-xl hover:bg-blue-400 hover:scale-110'>register</button>
