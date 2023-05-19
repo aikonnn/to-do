8f7fde58a3db9d8e23e2aee414bd18dd56330554
aikonnn <nokia_tuk@hotmail.com>
2023-05-19 16:01:06 +0700

    upd tests

--- tests/cypress/e2e/list_page.cy.js
+++ tests/cypress/e2e/list_page.cy.js
@@ -1,4 +1,4 @@
-describe('Authentication Pages', () => {
+describe('View Tasks', () => {
     it('login', () => {
       cy.visit('http://web:3000/')
       cy.url().should('include', '/login')
@@ -28,6 +28,46 @@ describe('Authentication Pages', () => {
 
         cy.contains('add new tasks').click()
         cy.get('#task-box').should('have.value', 'write your task here');
+
+        cy.get('#delete-btn').click()
+      })
+      it('new task with edit', () => {
+        cy.visit('http://web:3000/')
+        cy.url().should('include', '/login')
+    
+        cy.get('#email-input').type('new@email.com')
+        cy.get('#email-input').should('have.value', 'new@email.com')
+    
+        cy.get('#pass-input').type('newpass')
+        cy.get('#pass-input').should('have.value', 'newpass')
+    
+        cy.get('#login-btn').click();
+
+        cy.contains('add new tasks').click()
+        cy.get('#task-box').should('have.value', 'write your task here');
+        cy.get('#task-box').type('this is my task{enter}');
+        cy.get('#task-box').should('have.value', 'this is my task');
+
+        cy.get('#delete-btn').click()
+      })
+
+      it('new task with edit', () => {
+        cy.visit('http://web:3000/')
+        cy.url().should('include', '/login')
+    
+        cy.get('#email-input').type('new@email.com')
+        cy.get('#email-input').should('have.value', 'new@email.com')
+    
+        cy.get('#pass-input').type('newpass')
+        cy.get('#pass-input').should('have.value', 'newpass')
+    
+        cy.get('#login-btn').click();
+
+        cy.contains('add new tasks').click()
+        cy.get('#task-box').should('have.value', 'write your task here');
+
+        cy.get('#stat-btn').click();
+        cy.contains('Urgent').click();
 
         cy.get('#delete-btn').click()
       })
